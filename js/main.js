e084307390345be1cc4629eeae7d9b78c2fabf35
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-06-30 22:51:18 -0300

    Inserção do HTML e Javascript.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+for(var count = 1; count <= 100; count ++){
+   var f = count % 3 == 0,  b = count % 5 == 0;
+   console.log(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : count)
+}
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
